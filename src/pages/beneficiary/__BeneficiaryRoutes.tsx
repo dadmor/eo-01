4005eebb7b6cb5cb229b19c21ed8bedc84c0be5e
Grzesiek <dadmor@gmail.com>
2025-06-08 15:27:10 +0200

    0.0.6

--- src/pages/beneficiary/__BeneficiaryRoutes.tsx
+++ src/pages/beneficiary/__BeneficiaryRoutes.tsx
@@ -1,11 +1,9 @@
 // src/routes/AuthRoutes.tsx
 import { Route } from "react-router-dom";
-import { Login } from "./Login";
-import { Register } from "./Register";
+...
 
 export const AuthRoutes = () => (
   <>
-    <Route path="/login" element={<Login />} />
-    <Route path="/register" element={<Register />} />
+   ...
   </>
 );
